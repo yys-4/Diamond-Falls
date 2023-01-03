1a49ae372c0b08c704c11681b7d02bed58451e96
GaneshLindung <ganeshnusantara@gmail.com>
2023-01-03 21:34:34 +0700

    mainclass

---
+++ src/diamondfalls/DiamondFalls.java
@@ -0,0 +1,15 @@
+package diamondfalls;
+
+/**
+ * @author User
+ */
+public class DiamondFalls {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		new CteGame();
+	}
+
+}
