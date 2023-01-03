c013eeefa48d9514460b6a4e7ac689709e56b869
Bijas48 <tugassadrian@gmail.com>
2023-01-03 21:27:22 +0700

    adrian buat konek ke database

---
+++ src/diamondfalls/Koneksi.java
@@ -0,0 +1,16 @@
+package javaapplication13;
+
+import java.sql.Connection;
+
+/**
+ * @author DELL
+ */
+public class Koneksi {
+
+	private static Connection cn;
+
+	public static Connection getConnection() {
+		return null;
+	}
+
+}
