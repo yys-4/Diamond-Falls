c835b63f0f14e30741b6318e0ec8985755c82df5
GaneshLindung <ganeshnusantara@gmail.com>
2023-01-03 21:31:37 +0700

    Ganesh LeaderBoardPanel

---
+++ src/diamondfalls/LeaderBoardPanel.java
@@ -0,0 +1,32 @@
+package javaapplication13;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import java.awt.Image;
+import java.awt.Graphics;
+
+/**
+ * @author DELL
+ */
+public class LeaderBoardPanel extends JPanel {
+
+	JButton back = new JButton("Back");
+
+	JTable table;
+
+	Image helpbkg = new ImageIcon("./images/2.png").getImage();
+
+	public LeaderBoardPanel() {
+
+	}
+
+	void TableLb() {
+
+	}
+
+	public void paintComponent(Graphics g) {
+
+	}
+
+}
