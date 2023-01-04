4ede919b8a15ac54058ef1a2f8f3b042d9eb9247
yys-4 <61109623+yys-4@users.noreply.github.com>
2023-01-04 19:30:24 +0700

    Update DiamondFalls.java

--- src/diamondfalls/DiamondFalls.java
+++ src/diamondfalls/DiamondFalls.java
@@ -1,15 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package diamondfalls;
 
 /**
+ *
  * @author User
  */
 public class DiamondFalls {
 
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) {
-		new CteGame();
-	}
-
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        new TableLb();
+        
+        new CteGame();
+    }
+    
 }
