4b69d1c5eb11e20046d43f43b3b1861cd420009f
yys-4 <61109623+yys-4@users.noreply.github.com>
2023-01-04 19:33:30 +0700

    Update Koneksi.java

--- src/diamondfalls/Koneksi.java
+++ src/diamondfalls/Koneksi.java
@@ -1,16 +1,30 @@
-package javaapplication13;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package diamondfalls;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
+ *
  * @author DELL
  */
 public class Koneksi {
-
-	private static Connection cn;
-
-	public static Connection getConnection() {
-		return null;
-	}
-
+    private static Connection cn;
+    
+    public static Connection getConnection() throws SQLException{
+        String db = "jdbc:mysql://localhost:3306/gamepbo";
+        String user = "root";
+        String password = "";
+        
+        if (cn == null) {
+            cn = DriverManager.getConnection(db, user, password);
+            
+        }
+        
+        return cn;
+    }
 }
